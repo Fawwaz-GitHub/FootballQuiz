0eea33861e5f4c19cdb70777e40066b8075def96
Fawwaz-GitHub <102809557+Fawwaz-GitHub@users.noreply.github.com>
2022-07-12 12:15:24 +0530

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,112 @@
+const questions = [
+    {
+        number: 1,
+        question: 'Who Is The Top Goal-Scorer Of Champions League ?',
+        answer: 'Cristiano Ronaldo',
+        options: [
+            'Lionel Messi',
+            'Cristiano Ronaldo',
+            'Pele',
+            'Diego Maradona'
+        ],
+    },
+    {
+        number: 2,
+        question: 'Which Team Is Famous For Their PlayStyle Known As Tiki-Taka',
+        answer: 'Barcelona',
+        options: [
+            'Real Madrid',
+            'Valencia',
+            'Sevilla',
+            'Barcelona'
+        ],
+    },
+    {
+        number: 3,
+        question: 'Which Manager Has Won The Most Premier League Trophies ?',
+        answer: 'Sir Alex Ferguson',
+        options: [
+            'Pep Guardiola',
+            'Jose Mourinho',
+            'Sir Alex Ferguson',
+            'Carlos Ancelotti'
+        ],
+    },
+    {
+        number: 4,
+        question: 'Which Player Has Scored The Most Goals In a Calender Year ?',
+        answer: 'Lionel Messi',
+        options: [
+            'Cristiano Ronaldo',
+            'Ronaldinho',
+            'Lionel Messi',
+            'Robert Lewandowski'
+        ],
+    },
+    {
+        number: 5,
+        question: 'Which Teams Has Won The Most Number Of Champions League Title ?',
+        answer: 'Real Madrid',
+        options: [
+            'Bayern Munchen',
+            'Real Madrid',
+            'Barcelona',
+            'Liverpool'
+        ],
+    },
+    {
+        number: 6,
+        question: 'Pichichi Trophy Is Awarded To The Player Who Scored Most Goal In A Season Of Which League ?',
+        answer: 'La Liga',
+        options: [
+            'La Liga',
+            'Bundesliga',
+            'Premier League',
+            'Seria A'
+        ],
+    },
+    {
+        number: 7,
+        question: 'Who Is The Only Manager To Win The League Title Of All Top 5 Leagues ?',
+        answer: 'Carlos Ancelotti',
+        options: [
+            'Pep Guardiola',
+            'Jose Mourinho',
+            'None',
+            'Carlos Ancelotti'
+        ],
+    },
+    {
+        number: 8,
+        question: '"La Pulga" Is The Nick Name Of Which Player ?',
+        answer: 'Lionel Messi',
+        options: [
+            'Lionel Messi',
+            'David Villa',
+            'Luka Modric',
+            'Cesc Fabregas'
+        ],
+    },
+    {
+        number: 9,
+        question: 'Apart From Barcelona Which Is The Only Other Team To Win Sextuple ?',
+        answer: 'Bayern Munchen',
+        options: [
+            'Liverpool',
+            'Real Madrid',
+            'Bayern Munchen',
+            'Juventus'
+        ],
+    },
+    {
+        number: 10,
+        question: 'Which Player Has The Most Goal Contributions After Messi & Ronaldo ?',
+        answer: 'Luis Suarez',
+        options: [
+            'Zlatan Ibrahimovic',
+            'Luis Suarez',
+            'Neymar Jr',
+            'Robert Lewandowski'
+        ],
+    },
+]
